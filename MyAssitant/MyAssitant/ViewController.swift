fbcdbb2b46677ffefadad0a5de6b1ee0dcc23a23
Lizhuo You <mrlizhuoyou@gmail.com>
2020-03-05 01:01:27 -0500

    Prepare 0

---
+++ MyAssitant/MyAssitant/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyAssitant
+//
+//  Created by Lizhuo You on 2020-03-05.
+//  Copyright © 2020 Lizhuo You. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
